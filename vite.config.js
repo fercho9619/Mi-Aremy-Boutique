779ae77bcb898ea4f9e7a956cf3b397f72764761
fercho9619 <fernando.sahagun9@gmail.com>
2025-06-04 19:49:26 -0600

    quitar el crypto

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // necesario para que los recursos se resuelvan bien en GitHub Pages
   plugins: [react()],
 })
